35912c147f9674ba496d149a484beccf3f30835a
ukpong joseph <aniekanjoseph499@gmail.com>
2026-03-11 00:02:57 +0100

    Created Repo

---
+++ springwithroadmap/src/main/java/dev/roadmapspring/springwithroadmap/SpringwithroadmapApplication.java
@@ -0,0 +1,13 @@
+package dev.roadmapspring.springwithroadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithroadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithroadmapApplication.class, args);
+	}
+
+}
---
+++ springwithroadmap/src/test/java/dev/roadmapspring/springwithroadmap/SpringwithroadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roadmapspring.springwithroadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithroadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
